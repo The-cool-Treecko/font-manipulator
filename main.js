c530bb1fd795d473d9a3f9aa5397d682e71dc318
The cool Treecko <77108834+The-cool-Treecko@users.noreply.github.com>
2021-12-28 17:56:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+x = (screen.width/2)
+y = (screen.height/2)-260
+x2 = ((screen.height/2)/2)-200
+y2 = (screen.height/2)-192
+
 function setup() {
     canvas = createCanvas(460, 460)
-    canvas.center();
+    canvas.position(x,y);
+    video = createCapture(VIDEO);
+    video.position(x2,y2);
+    video.size(683,384);
+    classifier = ml5.poseNet(video,modelLoaded);
+    classifier.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Model is Loaded");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
 }
